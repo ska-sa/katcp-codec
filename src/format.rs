d10374579f90a682384e7aa3ed196477e0ffca7a
Bruce Merry <bmerry@sarao.ac.za>
2024-03-26 16:06:53 +0200

    Optimisations for format.rs
    It's using a lot of unsafe code with MaybeUninit, which makes me
    nervous.

--- src/format.rs
+++ src/format.rs
@@ -13,7 +13,10 @@
  * limitations under the License.
  */
 
+use enum_map::{enum_map, EnumMap};
+use once_cell::sync::OnceCell;
 use std::io::Write;
+use std::mem::MaybeUninit;
 
 use crate::message::{Message, MessageType};
 
@@ -22,14 +25,34 @@ where
     N: AsRef<[u8]>,
     A: AsRef<[u8]>,
 {
-    /// Serialize the message
-    pub fn write<T: Write>(&self, target: &mut T) -> std::io::Result<()> {
-        let type_symbol = match self.mtype {
+    fn type_symbol(mtype: MessageType) -> u8 {
+        match mtype {
             MessageType::Request => b'?',
             MessageType::Reply => b'!',
             MessageType::Inform => b'#',
-        };
-        target.write_all(std::slice::from_ref(&type_symbol))?;
+        }
+    }
+
+    fn escape_map() -> &'static EnumMap<u8, u8> {
+        static INSTANCE: OnceCell<EnumMap<u8, u8>> = OnceCell::new();
+        INSTANCE.get_or_init(|| {
+            enum_map! {
+                b'\r' => b'r',
+                b'\n' => b'n',
+                b'\t' => b't',
+                b'\x1B' => b'e',
+                b'\0' => b'0',
+                b'\\' => b'\\',
+                b' ' => b'_',
+                _ => 0,  // Marker for not needing an escape
+            }
+        })
+    }
+
+    /// Serialize the message
+    pub fn write<T: Write>(&self, target: &mut T) -> std::io::Result<()> {
+        let symbol = Self::type_symbol(self.mtype);
+        target.write_all(std::slice::from_ref(&symbol))?;
         target.write_all(self.name.as_ref())?;
         if let Some(mid) = self.mid {
             target.write_all(b"[")?;
@@ -69,6 +92,67 @@ where
         Ok(())
     }
 
+    #[inline]
+    #[must_use]
+    unsafe fn append_byte(target: &mut [MaybeUninit<u8>], value: u8) -> &mut [MaybeUninit<u8>] {
+        target[0].write(value);
+        target.get_unchecked_mut(1..)
+    }
+
+    #[inline]
+    #[must_use]
+    unsafe fn append_bytes<'a>(
+        target: &'a mut [MaybeUninit<u8>],
+        values: &[u8],
+    ) -> &'a mut [MaybeUninit<u8>] {
+        let len = values.len();
+        std::ptr::copy_nonoverlapping(
+            values.as_ptr(),
+            target.as_mut_ptr() as *mut u8,
+            values.len(),
+        );
+        target.get_unchecked_mut(len..)
+    }
+
+    /// Write the message into a buffer.
+    ///
+    /// # Safety
+    ///
+    /// The target must have size of at least [write_size].
+    pub unsafe fn write_unchecked(&self, mut target: &mut [MaybeUninit<u8>]) {
+        target = Self::append_byte(target, Self::type_symbol(self.mtype));
+        target = Self::append_bytes(target, self.name.as_ref());
+        if let Some(mid) = self.mid {
+            target = Self::append_byte(target, b'[');
+            let mut buffer = itoa::Buffer::new();
+            target = Self::append_bytes(target, buffer.format(mid).as_bytes());
+            target = Self::append_byte(target, b']');
+        }
+        let emap = Self::escape_map();
+        for argument in self.arguments.iter() {
+            let argument = argument.as_ref();
+            target = Self::append_byte(target, b' ');
+            if argument.is_empty() {
+                target = Self::append_bytes(target, b"\\@");
+            }
+            let mut pos = 0;
+            for &c in argument.iter() {
+                let esc = emap[c];
+                if esc == 0 {
+                    // No escaping is needed
+                    target[pos].write(c);
+                    pos += 1;
+                } else {
+                    target[pos].write(b'\\');
+                    target[pos + 1].write(esc);
+                    pos += 2;
+                }
+            }
+            target = &mut target[pos..];
+        }
+        let _ = Self::append_byte(target, b'\n');
+    }
+
     /// Get the number of bytes needed by [write].
     pub fn write_size(&self) -> usize {
         // Type symbol, name, spaces and newline
@@ -78,24 +162,26 @@ where
             let mid_formatted = buffer.format(mid);
             bytes += 2 + mid_formatted.len();
         }
+        let emap = Self::escape_map();
         for argument in self.arguments.iter() {
             let argument = argument.as_ref();
             if argument.is_empty() {
                 bytes += 2; // For the \@
             }
             for c in argument.iter() {
-                bytes += match c {
-                    b'\r' | b'\n' | b'\t' | b'\x1B' | b'\0' | b'\\' | b' ' => 2,
-                    _ => 1,
-                }
+                bytes += if emap[*c] != 0 { 2 } else { 1 };
             }
         }
         bytes
     }
 
     pub fn to_bytes(&self) -> Vec<u8> {
-        let mut vec = Vec::with_capacity(self.write_size());
-        self.write(&mut vec).unwrap(); // write to vec cannot fail
+        let size = self.write_size();
+        let mut vec = Vec::with_capacity(size);
+        unsafe {
+            self.write_unchecked(vec.spare_capacity_mut());
+            vec.set_len(size);
+        }
         vec
     }
 }
